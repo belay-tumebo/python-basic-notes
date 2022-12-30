516f9960677abf08fd03b0a5abe192120639de43
belay-tumebo <belaynehl@gmail.com>
2022-12-30 17:01:36 -0600

    first commit

---
+++ hello.py
@@ -0,0 +1,13 @@
+
+#name = 'Mede'
+#print(f'Hello, {name}!') # newest format, python 3.6+
+#print('Hello, {}'.format(name))
+#print('Hello, %s' %(name))
+    
+#if __name__ == '__main__':
+#    print(__name__)
+#    greet('Python')
+
+
+name = input("Please give me your name:\n")
+print(f'Hello, {name}!') # newest format, python 3.6+
